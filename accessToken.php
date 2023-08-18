9533b56b8b4b42b1ab5a9f69a501e8ab581c7a47
justus <justnyakundi@gmail.com>
2023-08-18 23:04:56 +0300

    adding my daraja api

--- accessToken.php
+++ accessToken.php
@@ -1,18 +1,28 @@
 <?php
-//YOU MPESA API KEYS
-$consumerKey = "1HUeXFGGGBzvq8AIeDqEAUa6g2biLwvw"; //Fill with your app Consumer Key
-$consumerSecret = "QpSvFnMg6GU4E4GE"; //Fill with your app Consumer Secret
-//ACCESS TOKEN URL
+// YOU MPESA API KEYS
+$consumerKey = "1HUeXFGGGBzvq8AIeDqEAUa6g2biLwvw"; // Fill with your app Consumer Key
+$consumerSecret = "QpSvFnMg6GU4E4GE"; // Fill with your app Consumer Secret
+// ACCESS TOKEN URL
 $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-$headers = ['Content-Type:application/json; charset=utf8'];
+$headers = array('Content-Type:application/json; charset=utf8');
 $curl = curl_init($access_token_url);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($curl, CURLOPT_HEADER, FALSE);
 curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
 $result = curl_exec($curl);
-$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-$result = json_decode($result);
- $access_token = $result->access_token;
- curl_close($curl);
 
+if ($result === false) {
+    echo "cURL error: " . curl_error($curl);
+} else {
+    $result = json_decode($result);
+    if ($result === null) {
+        echo "JSON decoding error: " . json_last_error_msg();
+    } else {
+        $access_token = $result->access_token;
+        echo " " . $access_token;
+    }
+}
+
+curl_close($curl);
+?>
